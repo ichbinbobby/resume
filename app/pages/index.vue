219852c1b4c95c4271c01c7392755443ef35e071
Nguyen, Thanh Binh <thanh-binh.nguyen@strato.de>
2025-09-17 12:34:43 +0200

    Added links to socials

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,18 @@
 <template>
   <UContainer>
-    <h1 class="text-primary text-4xl mt-4">Name</h1>
-    <h2 class="text-default text-2xl">Frontend Developer</h2>
+    <h1 class="text-primary text-4xl mt-4">Resume</h1>
+    <h2 class="text-default text-2xl">Thanh Binh Nguyen</h2>
 
-    <UCard class="mt-4">
+    <UCard class="mt-6">
       <template #header>
-        <span class="text-lg">Summary</span>
+        <div class="flex justify-between items-center">
+          <span class="text-lg">Summary</span>
+          
+          <div class="flex items-center gap-2">
+            <UButton icon="i-simple-icons-github" color="neutral" target="_blank" to="https://github.com/ichbinbobby" variant="subtle" />
+            <UButton icon="i-simple-icons-linkedin" color="neutral" target="_blank" to="https://www.linkedin.com/in/ichbinbobby/" variant="subtle" />
+          </div>
+        </div>
       </template>
 
       <p>
@@ -14,7 +21,7 @@
       </p>
     </UCard>
 
-    <UCard class="mt-4">
+    <UCard class="mt-6">
       <template #header>
         <span class="text-lg">Experience</span>
       </template>
@@ -36,7 +43,7 @@
 
       <p class="mt-2">
         Completed a three year apprenticeship in a dual education system. Switched between different departments at the company 
-        in order to learn various topics and technologies, including Webdesign, Backend Development, Frontend Development, and IT Support.
+        in order to learn various topics and technologies, including Webdesign, Backend Development, Frontend Development and IT Support.
       </p>
     </UCard>
   </UContainer>
